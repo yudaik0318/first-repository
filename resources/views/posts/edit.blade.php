77f4c73e2a58a67d58aaea1fba2ec9988e452050
yudaik0318 <yudaik0318@gmail.com>
2024-07-16 19:21:41 +0900

    ADD 初期設定&基礎実装

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    </head>
+    <body class="antialiased">
+        <h1>Blog Name</h1>
+        <form action="/posts/{{ $post->id }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name=post[title] placeholder="タイトル" value={{ $post->title }}>
+                <p class='title__error' style="color:red">{{ $errors->first('post.title') }}</p>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。">{{ $post->body }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+            </div>
+            <input type="submit" value="update">
+        </form>
+        <div class='footer'>
+            <a href="/posts/{{ $post->id }}">戻る</a>
+        </div>
+    </body>
+</html>
