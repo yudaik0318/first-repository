626a0f39cd3bd493d856b46531218a523854f64c
yudaik0318 <yudaik0318@gmail.com>
2024-07-16 14:13:34 +0900

    ADD 初期設定&基礎実装

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    </head>
+    <body class="antialiased">
+        <h1>Blog Name</h1>
+        <div class='posts'>
+            @foreach($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>{{ $post->title }}</h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class='paginate'>{{ $posts->links()}}</div>
+    </body>
+</html>
